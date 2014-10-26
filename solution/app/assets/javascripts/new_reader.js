8b6eac598937ee74cdfee521e902ae28ff894ddd
Ned Ruggeri <ruggeri@self-loop.com>
2014-10-25 22:00:19 -0700

    more formatting/normalization

--- solution/app/assets/javascripts/new_reader.js
+++ solution/app/assets/javascripts/new_reader.js
@@ -4,19 +4,19 @@ window.NewReader = {
     Views: {},
     Routers: {},
     initialize: function() {
-        var $rootEl = $('#content');
-        var $sidebar = $('#sidebar');
-        var feeds = new NewReader.Collections.Feeds();
-        feeds.fetch();
+      var $rootEl = $('#content');
+      var $sidebar = $('#sidebar');
+      var feeds = new NewReader.Collections.Feeds();
+      feeds.fetch();
 
-        // install the sidebar external to the router as it is
-        // independent of any routing
-        var feedsIndexView = new NewReader.Views.FeedsIndex({
-            collection: feeds
-        });
-        $sidebar.html(feedsIndexView.render().$el);
+      // install the sidebar external to the router as it is
+      // independent of any routing
+      var feedsIndexView = new NewReader.Views.FeedsIndex({
+        collection: feeds
+      });
+      $sidebar.html(feedsIndexView.render().$el);
 
-        new NewReader.Routers.FeedRouter(feeds, $rootEl);
-        Backbone.history.start();
+      new NewReader.Routers.FeedRouter(feeds, $rootEl);
+      Backbone.history.start();
     }
 };
